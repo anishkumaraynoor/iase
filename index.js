b58b26b27df2065d16afca2eacf609cd562b5de4
Anishkumar <anishpravitha@gmail.com>
2024-03-22 14:37:52 +0530

    second

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const iaseServer = express()
 iaseServer.use(cors())
 iaseServer.use(express.json())
-const PORT = 3000
+const PORT = 3000 || process.env.PORT
 iaseServer.listen(PORT, ()=>{
     console.log(`Server started Port:${PORT}`);
 })
